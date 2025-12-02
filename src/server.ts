9b4b0d111b96f8b8252dc26297cb0227171f8445
Didiercito <231193@ids.upchiapas.edu.mx>
2025-12-02 14:07:08 -0600

    fix

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,6 @@ import dotenv from 'dotenv';
 dotenv.config(); 
 
 import app from './app';
-// 1. Importar el consumidor
 import { KitchenRegisteredConsumer } from './infrastructure/events/KitchenRegisteredConsumer'; // <--- NUEVO
 
 const PORT = process.env.PORT || 3005;
@@ -11,9 +10,8 @@ async function startServer() {
   try {
     console.log('🚀 Iniciando Servidor de Pagos...');
     
-    // 2. Iniciar el escuchador de eventos
-    const consumer = new KitchenRegisteredConsumer(); // <--- NUEVO
-    await consumer.start();                           // <--- NUEVO
+    const consumer = new KitchenRegisteredConsumer(); 
+    await consumer.start();                           
 
     app.listen(PORT, () => {
       console.log(`🌐 Servidor corriendo en el puerto ${PORT}`);
